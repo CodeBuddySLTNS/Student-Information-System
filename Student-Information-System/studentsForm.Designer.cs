aa96e344754410245f6e4a058344cc193a30dec1
Renz Cole <renz2arjhay@gmail.com>
2025-09-08 08:57:26 +0800

    fix9

--- Student-Information-System/studentsForm.Designer.cs
+++ Student-Information-System/studentsForm.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
+            phoneTxt = new TextBox();
+            phoneLabel = new Label();
+            studentCodeTxt = new TextBox();
+            studentCodeLabel = new Label();
             lastNameTxt = new TextBox();
             middleNameTxt = new TextBox();
             lastNameLabel = new Label();
@@ -36,12 +40,16 @@
             middleNameLabel = new Label();
             firstNameLabel = new Label();
             groupBox2 = new GroupBox();
+            clearBtn = new Button();
             deleteStudentBtn = new Button();
             editStudentBtn = new Button();
             addStudentBtn = new Button();
             tableLayoutPanel1 = new TableLayoutPanel();
             dataGridView1 = new DataGridView();
             cancelBtn = new Button();
+            searchGroupBox = new GroupBox();
+            searchTxt = new TextBox();
+            searchLabel = new Label();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             tableLayoutPanel1.SuspendLayout();
@@ -50,6 +58,10 @@
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(phoneTxt);
+            groupBox1.Controls.Add(phoneLabel);
+            groupBox1.Controls.Add(studentCodeTxt);
+            groupBox1.Controls.Add(studentCodeLabel);
             groupBox1.Controls.Add(lastNameTxt);
             groupBox1.Controls.Add(middleNameTxt);
             groupBox1.Controls.Add(lastNameLabel);
@@ -58,9 +70,43 @@
             groupBox1.Controls.Add(firstNameLabel);
             groupBox1.Location = new Point(3, 3);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(495, 180);
+            groupBox1.Size = new Size(495, 250);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
+            // 
+            // phoneTxt
+            // 
+            phoneTxt.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            phoneTxt.Location = new Point(6, 198);
+            phoneTxt.Name = "phoneTxt";
+            phoneTxt.Size = new Size(483, 23);
+            phoneTxt.TabIndex = 7;
+            // 
+            // phoneLabel
+            // 
+            phoneLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            phoneLabel.Location = new Point(6, 172);
+            phoneLabel.Name = "phoneLabel";
+            phoneLabel.Size = new Size(483, 23);
+            phoneLabel.TabIndex = 0;
+            phoneLabel.Text = "Phone";
+            // 
+            // studentCodeTxt
+            // 
+            studentCodeTxt.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            studentCodeTxt.Location = new Point(6, 19);
+            studentCodeTxt.Name = "studentCodeTxt";
+            studentCodeTxt.Size = new Size(483, 23);
+            studentCodeTxt.TabIndex = 1;
+            // 
+            // studentCodeLabel
+            // 
+            studentCodeLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            studentCodeLabel.Location = new Point(6, -5);
+            studentCodeLabel.Name = "studentCodeLabel";
+            studentCodeLabel.Size = new Size(483, 23);
+            studentCodeLabel.TabIndex = 0;
+            studentCodeLabel.Text = "Student Code";
             // 
             // lastNameTxt
             // 
@@ -76,7 +122,7 @@
             middleNameTxt.Location = new Point(6, 94);
             middleNameTxt.Name = "middleNameTxt";
             middleNameTxt.Size = new Size(483, 23);
-            middleNameTxt.TabIndex = 5;
+            middleNameTxt.TabIndex = 4;
             // 
             // lastNameLabel
             // 
@@ -93,7 +139,7 @@
             firstNameTxt.Location = new Point(6, 40);
             firstNameTxt.Name = "firstNameTxt";
             firstNameTxt.Size = new Size(483, 23);
-            firstNameTxt.TabIndex = 5;
+            firstNameTxt.TabIndex = 3;
             // 
             // middleNameLabel
             // 
@@ -107,24 +153,37 @@
             // firstNameLabel
             // 
             firstNameLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            firstNameLabel.Location = new Point(6, 19);
+            firstNameLabel.Location = new Point(6, 14);
             firstNameLabel.Name = "firstNameLabel";
-            firstNameLabel.Size = new Size(483, 29);
+            firstNameLabel.Size = new Size(483, 23);
             firstNameLabel.TabIndex = 0;
             firstNameLabel.Text = "First Name";
             // 
             // groupBox2
             // 
             groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox2.Controls.Add(clearBtn);
             groupBox2.Controls.Add(cancelBtn);
             groupBox2.Controls.Add(deleteStudentBtn);
             groupBox2.Controls.Add(editStudentBtn);
             groupBox2.Controls.Add(addStudentBtn);
             groupBox2.Location = new Point(529, 3);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(302, 169);
+            groupBox2.Size = new Size(302, 239);
             groupBox2.TabIndex = 2;
             groupBox2.TabStop = false;
+            // 
+            // clearBtn
+            // 
+            clearBtn.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            clearBtn.BackColor = Color.Orange;
+            clearBtn.Location = new Point(6, 125);
+            clearBtn.Name = "clearBtn";
+            clearBtn.Size = new Size(290, 34);
+            clearBtn.TabIndex = 4;
+            clearBtn.Text = "Clear";
+            clearBtn.UseVisualStyleBackColor = false;
+            clearBtn.Click += clearBtn_Click;
             // 
             // deleteStudentBtn
             // 
@@ -175,16 +234,47 @@
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.Size = new Size(834, 186);
+            tableLayoutPanel1.Size = new Size(834, 250);
             tableLayoutPanel1.TabIndex = 3;
+            // 
+            // searchGroupBox
+            // 
+            searchGroupBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            searchGroupBox.Controls.Add(searchTxt);
+            searchGroupBox.Controls.Add(searchLabel);
+            searchGroupBox.Location = new Point(40, 294);
+            searchGroupBox.Name = "searchGroupBox";
+            searchGroupBox.Size = new Size(834, 60);
+            searchGroupBox.TabIndex = 4;
+            searchGroupBox.TabStop = false;
+            // 
+            // searchTxt
+            // 
+            searchTxt.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            searchTxt.Location = new Point(6, 30);
+            searchTxt.Name = "searchTxt";
+            searchTxt.Size = new Size(822, 23);
+            searchTxt.TabIndex = 1;
+            searchTxt.TextChanged += searchTxt_TextChanged;
+            // 
+            // searchLabel
+            // 
+            searchLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            searchLabel.Location = new Point(6, 10);
+            searchLabel.Name = "searchLabel";
+            searchLabel.Size = new Size(822, 23);
+            searchLabel.TabIndex = 0;
+            searchLabel.Text = "Search by Student Code or Name:";
             // 
             // dataGridView1
             // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(40, 242);
+            dataGridView1.Location = new Point(40, 360);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(834, 227);
-            dataGridView1.TabIndex = 4;
+            dataGridView1.Size = new Size(834, 209);
+            dataGridView1.TabIndex = 5;
+            dataGridView1.SelectionChanged += dataGridView1_SelectionChanged;
             // 
             // cancelBtn
             // 
@@ -202,12 +292,13 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(909, 498);
+            ClientSize = new Size(909, 580);
             Controls.Add(dataGridView1);
+            Controls.Add(searchGroupBox);
             Controls.Add(tableLayoutPanel1);
-            MinimumSize = new Size(756, 506);
+            MinimumSize = new Size(756, 588);
             Name = "studentsForm";
-            Text = "Users Form";
+            Text = "Student Management";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
@@ -227,9 +318,17 @@
         private Label middleNameLabel;
         private TextBox lastNameTxt;
         private Label lastNameLabel;
+        private TextBox phoneTxt;
+        private Label phoneLabel;
+        private TextBox studentCodeTxt;
+        private Label studentCodeLabel;
+        private Button clearBtn;
         private Button deleteStudentBtn;
         private Button editStudentBtn;
         private Button addStudentBtn;
+        private GroupBox searchGroupBox;
+        private TextBox searchTxt;
+        private Label searchLabel;
         private DataGridView dataGridView1;
         private Button cancelBtn;
     }
--- Student-Information-System/studentsForm.cs
+++ Student-Information-System/studentsForm.cs
@@ -12,10 +12,38 @@ namespace Student_Information_System
 {
     public partial class studentsForm : Form
     {
-        string action = "";
+        private string action = "";
+        private DataTable originalData;
+        private int selectedStudentId = -1;
+
         public studentsForm()
         {
             InitializeComponent();
+            LoadStudentData();
+            SetupDataGridView();
+        }
+
+        private void LoadStudentData()
+        {
+            try
+            {
+                originalData = Database.GetStudents();
+                dataGridView1.DataSource = originalData;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading student data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SetupDataGridView()
+        {
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
         }
 
         private void addStudentBtn_Click(object sender, EventArgs e)
@@ -28,16 +56,38 @@ namespace Student_Information_System
                 deleteStudentBtn.Enabled = false;
                 editStudentBtn.BackColor = Color.Silver;
                 deleteStudentBtn.BackColor = Color.Silver;
+                ClearFields();
             }
             else
             {
-                action = "";
-                addStudentBtn.Text = "Add";
-                editStudentBtn.Enabled = true;
-                deleteStudentBtn.Enabled = true;
-                editStudentBtn.BackColor = Color.Blue;
-                deleteStudentBtn.BackColor = Color.LightCoral;
-                MessageBox.Show("added", "Success!");
+                if (ValidateInput())
+                {
+                    try
+                    {
+                        int studentCode = int.Parse(studentCodeTxt.Text);
+                        string firstName = firstNameTxt.Text.Trim();
+                        string middleName = string.IsNullOrWhiteSpace(middleNameTxt.Text) ? null : middleNameTxt.Text.Trim();
+                        string lastName = lastNameTxt.Text.Trim();
+                        string phone = string.IsNullOrWhiteSpace(phoneTxt.Text) ? null : phoneTxt.Text.Trim();
+
+                        int result = Database.AddStudent(studentCode, firstName, middleName, lastName, phone);
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Student added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            LoadStudentData();
+                            ResetButtons();
+                            ClearFields();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to add student.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error adding student: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
             }
         }
 
@@ -45,6 +95,11 @@ namespace Student_Information_System
         {
             if (action == "")
             {
+                if (selectedStudentId == -1)
+                {
+                    MessageBox.Show("Please select a student to edit.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
                 action = "Edit";
                 editStudentBtn.Text = "Save";
                 addStudentBtn.Enabled = false;
@@ -55,43 +110,178 @@ namespace Student_Information_System
             }
             else
             {
-                action = "";
-                editStudentBtn.Text = "Edit";
-                addStudentBtn.Enabled = true;
-                deleteStudentBtn.Enabled = true;
-                addStudentBtn.BackColor = Color.LightGreen;
-                editStudentBtn.BackColor = Color.Blue;
-                deleteStudentBtn.BackColor = Color.LightCoral;
-                MessageBox.Show("edited", "Success!");
+                if (ValidateInput())
+                {
+                    try
+                    {
+                        int studentCode = int.Parse(studentCodeTxt.Text);
+                        string firstName = firstNameTxt.Text.Trim();
+                        string middleName = string.IsNullOrWhiteSpace(middleNameTxt.Text) ? null : middleNameTxt.Text.Trim();
+                        string lastName = lastNameTxt.Text.Trim();
+                        string phone = string.IsNullOrWhiteSpace(phoneTxt.Text) ? null : phoneTxt.Text.Trim();
+
+                        int result = Database.UpdateStudent(selectedStudentId, studentCode, firstName, middleName, lastName, phone);
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Student updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            LoadStudentData();
+                            ResetButtons();
+                            ClearFields();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to update student.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error updating student: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
             }
         }
 
         private void deleteStudentBtn_Click(object sender, EventArgs e)
         {
-            if (action == "")
+            if (selectedStudentId == -1)
             {
-                action = "Delete";
-                deleteStudentBtn.Text = "Save";
-                addStudentBtn.Enabled = false;
-                editStudentBtn.Enabled = false;
-                addStudentBtn.BackColor = Color.Silver;
-                deleteStudentBtn.BackColor = Color.LightGreen;
-                editStudentBtn.BackColor = Color.Silver;
+                MessageBox.Show("Please select a student to delete.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
             }
-            else
+
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to delete this student? This action cannot be undone.",
+                "Confirm Delete",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
             {
-                action = "";
-                deleteStudentBtn.Text = "Delete";
-                addStudentBtn.Enabled = true;
-                editStudentBtn.Enabled = true;
-                addStudentBtn.BackColor = Color.LightGreen;
-                editStudentBtn.BackColor = Color.Blue;
-                deleteStudentBtn.BackColor = Color.LightCoral;
-                MessageBox.Show("deleted", "Success!");
+                try
+                {
+                    int deleteResult = Database.DeleteStudent(selectedStudentId);
+                    if (deleteResult > 0)
+                    {
+                        MessageBox.Show("Student deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        LoadStudentData();
+                        ClearFields();
+                        selectedStudentId = -1;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to delete student.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error deleting student: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
 
         private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            ResetButtons();
+            ClearFields();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                selectedStudentId = Convert.ToInt32(selectedRow.Cells["id"].Value);
+                
+                studentCodeTxt.Text = selectedRow.Cells["studentCode"].Value.ToString();
+                firstNameTxt.Text = selectedRow.Cells["firstName"].Value.ToString();
+                middleNameTxt.Text = selectedRow.Cells["middleName"].Value?.ToString() ?? "";
+                lastNameTxt.Text = selectedRow.Cells["lastName"].Value.ToString();
+                phoneTxt.Text = selectedRow.Cells["phone"].Value?.ToString() ?? "";
+            }
+        }
+
+        private void searchTxt_TextChanged(object sender, EventArgs e)
+        {
+            if (originalData != null)
+            {
+                string searchText = searchTxt.Text.ToLower();
+                if (string.IsNullOrWhiteSpace(searchText))
+                {
+                    dataGridView1.DataSource = originalData;
+                }
+                else
+                {
+                    DataTable filteredData = originalData.Clone();
+                    foreach (DataRow row in originalData.Rows)
+                    {
+                        string studentCode = row["studentCode"].ToString().ToLower();
+                        string firstName = row["firstName"].ToString().ToLower();
+                        string middleName = row["middleName"].ToString().ToLower();
+                        string lastName = row["lastName"].ToString().ToLower();
+                        string fullName = $"{firstName} {middleName} {lastName}".ToLower();
+
+                        if (studentCode.Contains(searchText) || 
+                            firstName.Contains(searchText) || 
+                            middleName.Contains(searchText) || 
+                            lastName.Contains(searchText) ||
+                            fullName.Contains(searchText))
+                        {
+                            filteredData.ImportRow(row);
+                        }
+                    }
+                    dataGridView1.DataSource = filteredData;
+                }
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(studentCodeTxt.Text))
+            {
+                MessageBox.Show("Student Code is required.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                studentCodeTxt.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(firstNameTxt.Text))
+            {
+                MessageBox.Show("First Name is required.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                firstNameTxt.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(lastNameTxt.Text))
+            {
+                MessageBox.Show("Last Name is required.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                lastNameTxt.Focus();
+                return false;
+            }
+
+            if (!int.TryParse(studentCodeTxt.Text, out _))
+            {
+                MessageBox.Show("Student Code must be a valid number.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                studentCodeTxt.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ClearFields()
+        {
+            studentCodeTxt.Clear();
+            firstNameTxt.Clear();
+            middleNameTxt.Clear();
+            lastNameTxt.Clear();
+            phoneTxt.Clear();
+        }
+
+        private void ResetButtons()
         {
             action = "";
             addStudentBtn.Text = "Add";
